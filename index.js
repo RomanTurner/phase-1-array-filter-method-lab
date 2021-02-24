348e6f43268466e7f4d86efd2ac427515ae86719
Roman Turner <rn.turner@live.com>
2021-02-23 17:05:45 -0800

    assingment complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+
+let findMatching = (array, name) =>
+  array.filter((target) => target.toLowerCase() === name.toLowerCase());
+
+
+let fuzzyMatch = (array, partOfName) => array.filter((target) => target.substring(0,2) === partOfName); 
+
+
+let matchName = (array, name) => array.filter((target) => target.name === name)
+    
+
